9e5b2dc184fab8867aa6686276ab5f73dfe33339
Bradley Jinks <bradley.jinks123@outlook.com>
2023-03-21 13:20:40 +0000

    Testing

--- main.tf
+++ main.tf
@@ -1,27 +1,17 @@
 terraform {
-  required_version = "~> 1.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.5"
-    }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = "~> 1.14"
-    }
   }
 }
 
 provider "aws" {
-  region = "eu-west-1"
+  region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "b" {
+resource "aws_s3_bucket" "c" {
   bucket = "bj-test-bucket"
 
   tags = {
@@ -30,21 +20,6 @@ resource "aws_s3_bucket" "b" {
   }
 }
 
-resource "aws_s3_bucket" "bb" {
-  bucket = "bj-test-bucket2"
-
-  tags = {
-    Name        = "My bucket 2"
-    Environment = "Dev"
-  }
-}
-
-variable "subject" {
-   type = string
-   default = "World"
-   description = "Subject to hello"
-}
-
-output "hello_world" {
-  value = "Hello, ${var.subject}! Cool Beans"
+output "bucket_arn" {
+  value = aws_s3_bucket.c.arn
 }
