bbbaf728ca66fa0891cd728ea2700f437d0fa436
Bradley Jinks <bradley.jinks123@outlook.com>
2023-03-21 11:25:02 +0000

    Added bucket to test syncing

--- main.tf
+++ main.tf
@@ -1,5 +1,33 @@
 terraform {
-  required_version = ">= 0.12.26"
+  required_version = "~> 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.5"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "bj-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
 
 variable "subject" {
